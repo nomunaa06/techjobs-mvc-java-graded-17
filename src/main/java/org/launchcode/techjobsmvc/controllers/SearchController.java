2dafb56e12db0c24ffc6a615e27e75362286c8c6
Nomundari <nomunaa@gmail.com>
2024-11-13 00:12:57 -0600

    displaySearchResults needs to be fixed

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,9 +29,17 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @GetMapping(value ="jobs")
-    public String displaySearchResults(Model model){
-
+    @PostMapping(value ="results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if(searchType.equals("all")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs With All");
+        }
+        else{
+            jobs = JobData.findByValue(searchTerm);
+        }
+        model.addAttribute("jobs",jobs);
         return "search";
     }
 
